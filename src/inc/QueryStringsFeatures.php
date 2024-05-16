93cf84ba252269bee6c8f044b7d66fa342364a05
Sandy Figueroa <sandyfigueroa@users.noreply.github.com>
2024-05-16 12:56:56 -0500

    QueryStrings are now option names in WP Rocket's code base and can be user like ?delay_js=0 / ?delay_js=1 to deactivate or activate

--- src/inc/QueryStringsFeatures.php
+++ src/inc/QueryStringsFeatures.php
@@ -12,25 +12,25 @@ class QueryStringFeatures
     }
     public function init()
     {
-        $this->disable_all_options();
-        $this->enable_all_options();
-        $this->disable_options();
-        $this->enable_options();
+        $this->deactivate_all_options();
+        $this->activate_all_options();
+        $this->disable_or_enable_cache();
+        $this->disable_or_enable_options();
     }
-    private function disable_all_options()
+    private function deactivate_all_options()
     {
-        if (!isset($_GET[get_no_query_string_prefix() . 'all'])) return;
-        $option_list = get_wpr_no_prefixed_options();
+        if (!isset($_GET['wpr_deactivate_all'])) return;
+        $option_list = get_option_list();
         add_filter('do_rocket_generate_caching_files', '__return_false');
         foreach ($option_list as $option) {
             add_filter('get_rocket_option_' . $option, '__return_zero');
         }
     }
 
-    private function enable_all_options()
+    private function activate_all_options()
     {
-        if (!isset($_GET[get_activate_query_string_prefix() . 'all'])) return;
-        $option_list = get_wpr_activate_prefixed_options();
+        if (!isset($_GET['wpr_activate_all'])) return;
+        $option_list = get_option_list();
         add_filter('do_rocket_generate_caching_files', '__return_true');
         foreach ($option_list as $option) {
             add_filter('get_rocket_option_' . $option, function () {
@@ -38,29 +38,28 @@ class QueryStringFeatures
             });
         }
     }
-
-    private function disable_options()
+    private function disable_or_enable_cache()
     {
-        $option_list = get_wpr_no_prefixed_options();
-        if (isset($_GET[get_no_query_string_prefix() . 'cache'])) {
-            add_filter('do_rocket_generate_caching_files', '__return_false');
-        }
-        foreach ($option_list as $query_string => $option) {
-            if (isset($_GET[$query_string])) {
-                add_filter('get_rocket_option_' . $option, '__return_zero');
+        if (isset($_GET['wpr_cache'])) {
+            if ($_GET['wpr_cache'] === "1") {
+                add_filter('do_rocket_generate_caching_files', '__return_true');
+            } else if ($_GET['wpr_cache'] === "0") {
+                add_filter('do_rocket_generate_caching_files', '__return_false');
             }
         }
     }
-    private function enable_options()
+    private function disable_or_enable_options()
     {
-        $option_list = get_wpr_activate_prefixed_options();
-        if (isset($_GET[get_activate_query_string_prefix() . 'cache'])) {
-            add_filter('do_rocket_generate_caching_files', '__return_true');
-        }
-        foreach ($option_list as $query_string => $option) {
-            if (isset($_GET[$query_string])) {
-                add_filter('get_rocket_option_' . $option, function () {
-                    return 1;
+        $option_list = get_option_list();
+        foreach ($option_list as $option) {
+            if (isset($_GET[$option])) {
+                $value = $_GET[$option];
+                if ($value !== "1" && $value !== "0") {
+                    continue;
+                }
+                $value = intval($value);
+                add_filter('get_rocket_option_' . $option, function () use ($value) {
+                    return $value;
                 });
             }
         }
